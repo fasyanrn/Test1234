776ff54a64ba75a47de6275ed8b7e33d011cf561
Muhammad Fachreza Anggana Sukma <rezaanggana424@gmail.com>
2025-04-16 12:07:51 +0700

    Auth Controller Login + Regis

--- src/main/java/id/co/bsi/hello_spring/controller/AuthController.java
+++ src/main/java/id/co/bsi/hello_spring/controller/AuthController.java
@@ -1,22 +1,32 @@
 package id.co.bsi.hello_spring.controller;
 
 import id.co.bsi.hello_spring.dto.request.LoginRequest;
+import id.co.bsi.hello_spring.dto.request.RegisterRequest;
 import id.co.bsi.hello_spring.dto.response.LoginResponse;
-import org.apache.coyote.Response;
+import id.co.bsi.hello_spring.dto.response.RegisterResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoginController {
+public class AuthController {
 
     @PostMapping("/api/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = new LoginResponse();
         loginResponse.setStatus("success");
-        loginResponse.setToken("u21n3s9709281091");
+        loginResponse.setToken("wfweoi3hoihewihfero");
 
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/api/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
+        RegisterResponse registerResponse = new RegisterResponse();
+        registerResponse.setStatus("success");
+        registerResponse.setMessage("Registration Successful!");
+
+        return ResponseEntity.ok(registerResponse);
     }
 }
